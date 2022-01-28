7332c26b9f8a2157790ae79627cd5bd9197bf516
wingky12 <wingky12@gmail.com>
2022-01-28 13:43:53 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+echo "masuk";
+echo "</br>";
+echo "mantap";
+echo "</br>";
+echo "bro";
+echo "</br>";
+echo "joss mantap </br>";
+echo "Alhamdulilah ";
+echo "keren <\br>";
+echo "upt tik";
+echo "</br>";
+echo "ok om";
+echo "ALhamdulillah keren cuy";
+ ?>
